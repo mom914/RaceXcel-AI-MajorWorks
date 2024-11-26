389cd1ba272b66a380f59aa9aa535f34910c6b30
mom914 <136532485+mom914@users.noreply.github.com>
2024-11-26 14:27:37 +1100

    Machine learning update

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def Hello_World(self):
-    print(self)
+import pygame
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
 
-Hello_World("print")
+if torch.cuda.is_available:
+    device = "cuda"
+elif torch.mps.is_available:
+    device = "mps"
+else:
+    device = "cpu"
+print(f" using: {device}")
